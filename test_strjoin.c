faaf438cfb863ed234ec8792785b6c40203e0ffe
FirePh0enix <leonard@delbecq.net>
2023-11-09 17:18:46 +0100

    Add shebang to test.py

--- test_strjoin.c
+++ test_strjoin.c
@@ -6,5 +6,9 @@ void test_strjoin()
 {
 	START_TEST(strjoin);
 	TEST_RETURN_STRCMP_SINGLE(strjoin, ft_strjoin("Hello ", "world~"), "Hello world~");
+
+	ft_strjoin(NULL, "A");
+	ft_strjoin("A", NULL);
+	ft_strjoin(NULL, NULL);
 	END_TEST(strjoin);
 }
